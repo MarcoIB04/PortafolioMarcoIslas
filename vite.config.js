41823a20e09102ce56ff1dd470aab7752493c901
Marco <marcoib0401@outlook.com>
2026-02-04 22:44:40 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/PortafolioMarcoIslas/",
   plugins: [react()],
   // Necesario para GitHub Pages si el sitio está en https://usuario.github.io/mi-portafolio/
   // Si tu repo se llama distinto, cambia 'mi-portafolio' por el nombre del repo
-  base: '/mi-portafolio/',
 })
